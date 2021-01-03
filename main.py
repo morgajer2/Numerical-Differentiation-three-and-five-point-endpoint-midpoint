cac0876079181c7ba1ae5a9faf6023b9624e817b
Mor Gajer <morgajer2@gmail.com>
2021-01-03 18:37:10 +0200

    added user's inputs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from sympy import *
+
 x = Symbol('x')
 
 def threePointEndpoint(f, x0, h):
@@ -32,3 +33,17 @@ def fivePointEndpoint(f, x0, h):
     return ((-25) * g[x0] + 48 * g[x0 + h] + (- 36) * g[x0 + 2 * h] + 16 * g[
         x0 + 3 * h] + (- 3) * g[x0 + 4 * h]) / (12 * h)
 
+
+f = x ** 3 - 8 * (x ** 2) + 3
+x0 = float(input("please enter x0: "))
+h = float(input("please enter h: "))
+choise = int(input(
+    "which method do you want?\n1.Three Point Endpoint\n2.Three Point Midpoint\n3.Five Point Midpoint\n4.Five Point Endpoint"))
+if choise == 1:
+    print("f'(x0) = "+str(threePointEndpoint(f, x0, h)))
+elif choise == 2:
+    print("f'(x0) = "+str(threePointMidpoint(f, x0, h)))
+elif choise == 3:
+    print("f'(x0) = " + str(fivePointMidpoint(f, x0, h)))
+elif choise == 4:
+    print("f'(x0) = "+str(fivePointEndpoint(f, x0, h)))
